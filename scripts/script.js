5046a872d12760d199b6dd45ab1987b580d4c7a5
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-10-15 21:01:28 -0400

    Final v1.0

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+const shareIcon = document.getElementById('share');
+const shareDiv = document.getElementById('socialDiv');
+const disappear = document.getElementById('disappear');
+const info = document.getElementById('info');
+const mediaQuery = window.matchMedia('(max-width: 1000px)');
+
+shareIcon.addEventListener('mouseover', function(){
+ 
+});
+
+function handleSizeChange(e) {
+    if (e.matches) {
+        shareDiv.addEventListener('mouseenter', function(){
+            disappear.style.display = "block";
+            info.style.display ='none';
+        });
+        shareDiv.addEventListener('mouseleave', function(){
+            disappear.style.display = "none";
+            info.style.display ='grid';
+        });
+        console.log(mediaQuery);
+    }else{
+        shareIcon.addEventListener('mouseenter', function(){
+            
+        });
+        console.log(mediaQuery);
+        
+    }
+}
+
+mediaQuery.addEventListener('change', handleSizeChange)
+
+handleSizeChange(mediaQuery)
+
+
